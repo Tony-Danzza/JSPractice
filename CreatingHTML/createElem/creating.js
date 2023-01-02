6b6ea87612abf1e9465d5d4c46e09ebeaa090b9f
Adam McWhorter <adamm2bits@hotmail.com>
2023-01-01 19:51:06 -0800

    JS practice

--- CreatingHTML/createElem/creating.js
+++ CreatingHTML/createElem/creating.js
@@ -34,7 +34,7 @@ const dogs = ['Finn', 'Tito', 'Stella', 'Sadie']
 
 const myList = document.createElement("ul")
 
-myList.classList.add('container')
+myList.classList.add('animals')
 
 for (let i = 0; i < dogs.length; i++){
     const dogList = document.createElement('li')
@@ -47,4 +47,40 @@ const item = document.createElement('h1')
 
 item.textContent = dogs.forEach(dog => {
     console.log(dog)
+    
+})
+
+document.body.appendChild(item)
+
+
+const height = 800
+const width = 600
+const src = 'https://picsum.photos/'
+
+const alt = "Random item from picsum"
+
+const myHtml = `
+<div class="wrapper">    
+<img src="${src}/${width}/${height}/" alt="${alt}">
+</div>`
+
+// myHtml will be inserted as a string
+// so you will not be able to add a class attribute or anything similar to it like this.
+
+
+console.log(typeof(myHtml))
+
+const container = document.querySelector('.container')
+
+// would add it to the items inside the container
+// container.innerHTML += myHtml
+// would include it with items already inside the container
+container.innerHTML = myHtml
+
+
+// NOTE: you can still access the element from the DOM and then work with it like normal HTML
+const img = document.querySelector('.wrapper img')
+
+img.addEventListener('click', function () {
+    img.classList.toggle('round')
 })
--- CreatingHTML/rangeFragment/main.js
+++ CreatingHTML/rangeFragment/main.js
@@ -4,28 +4,21 @@ const src = 'https://picsum.photos/'
 
 const alt = "Random item from picsum"
 
-const myHtml = `
-<div class="wrapper">    
+const myHtml = `<div class="wrapper">
 <img src="${src}/${width}/${height}/" alt="${alt}">
 </div>`
 
-// myHtml will be inserted as a string
-// so you will not be able to add a class attribute or anything similar to it like this.
+// const container = document.querySelector('.container')
 
-
-console.log(typeof(myHtml))
-
-const container = document.querySelector('.container')
-
-// would add it to the items inside the container
 // container.innerHTML += myHtml
-// would include it with items already inside the container
-container.innerHTML = myHtml
+
+const myFrag = document.createRange().createContextualFragment(myHtml)
+
+console.log(myFrag);
 
 
-// NOTE: you can still access the element from the DOM and then work with it like normal HTML
-const img = document.querySelector('.wrapper img')
+console.log(myFrag.querySelector('img'))
 
-img.addEventListener('click', function () {
-    img.classList.toggle('round')
-})
+console.log(myFrag)
+
+document.body.appendChild(myFrag)
