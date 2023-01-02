c7c2b26d5e4c465671801e9c0b70be92503f38f8
Adam McWhorter <adamm2bits@hotmail.com>
2023-01-01 23:45:03 -0800

    JS events

--- Nodes/traversRemove/main.js
+++ Nodes/traversRemove/main.js
@@ -1,3 +1,9 @@
 const wes = document.querySelector('.wes')
 
 console.log(wes);
+console.log(wes.lastElementChild)
+console.log(wes.firstElementChild)
+console.log(wes.previousElementSibling);
+console.log(wes.nextElementSibling);
+console.log(wes.parentElement);
+console.log(wes.childNodes);
---
+++ events/listener/events.js
@@ -0,0 +1,56 @@
+const width = window.innerWidth
+const height = window.innerHeight
+console.log(width/height)
+        
+
+const butts = document.querySelector('.butts')
+
+butts.addEventListener('click', function () {
+    console.log('clicked')
+})
+
+
+function handleClicked() {
+    console.log("It got Clicked!")
+}
+
+// butts.addEventListener('dblclick', handleClicked)
+butts.removeEventListener('click', handleClicked)
+
+const coolBtn = document.querySelector('.cool')
+
+// coolBtn.addEventListener('click', handleClicked)
+coolBtn.removeEventListener('click', handleClicked)
+
+
+const hooray = () => console.log("Hooray!");
+
+
+const round = () => {
+    if (coolBtn.getAttribute('style') == 'background-color:#fff;') {
+        coolBtn.setAttribute('style', "background-color:powderblue;")
+        
+    } else {
+        coolBtn.setAttribute('style', "background-color:#fff;")
+    }
+    
+
+}
+coolBtn.addEventListener('click', round)
+
+
+const buyBtns = document.querySelectorAll('button.buy')
+
+
+console.log(buyBtns.forEach((buyBtn, i) => {
+    console.log(buyBtn.innerHTML);
+    console.log(buyBtn, i);
+    buyBtn.addEventListener('click', function() {
+        console.log(`Buying Item ${i + 1}`)
+    })
+}))
+
+
+function buyItem(num){
+    console.log(`Buying Item ${num}`)
+}
