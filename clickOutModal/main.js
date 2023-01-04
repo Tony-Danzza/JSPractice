765bcb1630b5c064931f99c4f8dfadeba6067645
Adam McWhorter <adamm2bits@hotmail.com>
2023-01-03 20:56:01 -0800

    outside click modal

--- clickOutModal/main.js
+++ clickOutModal/main.js
@@ -1,15 +1,29 @@
 const cardButtons = document.querySelectorAll('.card button')
 
+const modalOuter = document.querySelector('.modal-outer')
+const modalInner = document.querySelector('.modal-inner')
+
+
+
 function handleCardClick(e) {
     const button = e.currentTarget
     const card = button.closest('.card')
-    console.log(card);
+    const name = card.querySelector('h2').textContent
+    // console.log(card);
     const imgSrc = document.querySelector('img').src
-    console.log(imgSrc)
+    // console.log(imgSrc)
     const desc = card.dataset.description
-    console.log(desc);
-    console.log("Yay! clicked it!")
+    // console.log(desc);
+    // console.log("Yay! clicked it!")
+    modalInner.innerHTML =
+    `<img src="${imgSrc.replace('200', '600')}" alt="${name}">
+    <p>${desc}</p>`
+    modalOuter.classList.add('open')
 }
+
+// function closeModal() {
+//     modalOuter.classList.
+// }
 
 cardButtons.forEach(button => {
     button.addEventListener('click', handleCardClick)
