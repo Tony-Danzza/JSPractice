a3aed1004120c3eb99e0ef911d8c71965ffdd04a
Adam McWhorter <adamm2bits@hotmail.com>
2023-01-08 19:05:23 -0800

    slidder 1/2 way

--- slider/slider.js
+++ slider/slider.js
@@ -1,6 +1,6 @@
 function Slider(slider) {
     // console.log(slider)
-    console.log(slider instanceof(Element));
+    // console.log(slider instanceof(Element));
     // NEEDS TO BE WRAPPED IN () for bang to work right
     if (!(slider instanceof Element)) {
         throw new Error("No valid Slider found on page!")
@@ -9,13 +9,22 @@ function Slider(slider) {
     let current;
     let nextEl;
     let prevEl;
-    
-    const slides = slider.querySelectorAll(".slide")
+
+    const slides = slider.querySelector(".slides")
     const nextBtn = document.querySelector('.go-to-next')
     const prevBtn = document.querySelector('.go-to-prev')
-    console.log(prevBtn, nextBtn);
-    
-    // slides.forEach((slide) => console.log(slide.textContent))
+    // console.log(prevBtn, nextBtn);
+    // console.log(slides.firstElementChild)
+    function startSlider() {
+        current = slider.querySelector('.current') || slides.firstElementChild
+        console.log(current)
+        prevEl = current.previousElementSibling
+        nextEl = current.nextElementSibling
+        prevEl.
+        console.log(prevEl);
+    }
+
+    startSlider()
 }
 // const test = 
 const mainSlider = Slider(document.querySelector('.main-slider'))
