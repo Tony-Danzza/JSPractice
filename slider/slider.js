e272d11e8f05fa3e36ace1dd1c97f9e545d2452d
Adam McWhorter <adamm2bits@hotmail.com>
2023-01-08 20:44:14 -0800

    slider

--- slider/slider.js
+++ slider/slider.js
@@ -6,9 +6,9 @@ function Slider(slider) {
         throw new Error("No valid Slider found on page!")
     }
 
+    let prevEl;
     let current;
     let nextEl;
-    let prevEl;
 
     const slides = slider.querySelector(".slides")
     const nextBtn = document.querySelector('.go-to-next')
@@ -17,14 +17,36 @@ function Slider(slider) {
     // console.log(slides.firstElementChild)
     function startSlider() {
         current = slider.querySelector('.current') || slides.firstElementChild
-        console.log(current)
-        prevEl = current.previousElementSibling
-        nextEl = current.nextElementSibling
-        prevEl.
-        console.log(prevEl);
+        // console.log(current)
+        prevEl = current.previousElementSibling || slides.lastElementChild
+        nextEl = current.nextElementSibling || slides.firstElementChild
+        console.log(!!prevEl, !!nextEl, "TestME");
+    }
+    
+    function applyClasses() {
+        prevEl.classList.add('prev')
+        current.classList.add('current')
+        nextEl.classList.add('next')
+
     }
 
+    function move(direction) {
+        // define an array of a classes to remove and use spread operator to apply
+        const classesToRemove = ['prev', 'current', 'next']
+        // NOTE: pass an array of item to remove class values from and loop over with forEach. Where el will = each item in the array
+        [prevEl, current, nextEl].forEach((el) => (el.classList.remove(...classesToRemove)));
+        // prevEl.classList.remove(...classesToRemove)
+        // current.classList.remove(...classesToRemove)
+        // nextEl.classList.remove(...classesToRemove)
+        if (direction === 'back') {
+            
+            
+        }
+    }
+    
+    // Functions to run upon calling Slider()
     startSlider()
+    applyClasses()
 }
 // const test = 
 const mainSlider = Slider(document.querySelector('.main-slider'))
