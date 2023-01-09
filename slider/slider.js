6763f5f4704afa61e3ea9edd1742dc67e9fe719e
Adam McWhorter <adamm2bits@hotmail.com>
2023-01-08 21:43:54 -0800

    slider done!

--- slider/slider.js
+++ slider/slider.js
@@ -11,8 +11,8 @@ function Slider(slider) {
     let nextEl;
 
     const slides = slider.querySelector(".slides")
-    const nextBtn = document.querySelector('.go-to-next')
-    const prevBtn = document.querySelector('.go-to-prev')
+    const nextBtn = slider.querySelector('.go-to-next')
+    const prevBtn = slider.querySelector('.go-to-prev')
     // console.log(prevBtn, nextBtn);
     // console.log(slides.firstElementChild)
     function startSlider() {
@@ -34,19 +34,40 @@ function Slider(slider) {
         // define an array of a classes to remove and use spread operator to apply
         const classesToRemove = ['prev', 'current', 'next']
         // NOTE: pass an array of item to remove class values from and loop over with forEach. Where el will = each item in the array
-        [prevEl, current, nextEl].forEach((el) => (el.classList.remove(...classesToRemove)));
-        // prevEl.classList.remove(...classesToRemove)
-        // current.classList.remove(...classesToRemove)
-        // nextEl.classList.remove(...classesToRemove)
+        // [prevEl, nextEl, current].forEach((el) => (el.classList.remove(...classesToRemove)));
+        prevEl.classList.remove(...classesToRemove)
+        current.classList.remove(...classesToRemove)
+        nextEl.classList.remove(...classesToRemove)
         if (direction === 'back') {
+            prevEl = prevEl.previousElementSibling || slides.lastElementChild
+            current = current.previousElementSibling || slides.lastElementChild
+            nextEl = nextEl.previousElementSibling || slides.lastElementChild
             
+            // console.log(`GOING BACK: Prev ${prevEl.innerText}, Current ${current.innerText}, next ${nextEl.innerText}`)
+            // prevEl.classList.add('prev')
+            // current.classList.add('current')
+            // nextEl.classList.add('next')
+        } 
+        if (direction === "forward") {
+            prevEl = prevEl.nextElementSibling || slides.firstElementChild
+            current = current.nextElementSibling || slides.firstElementChild
+            nextEl = nextEl.nextElementSibling || slides.firstElementChild
             
         }
+        prevEl.classList.add('prev')
+        current.classList.add('current')
+        nextEl.classList.add('next')
     }
-    
     // Functions to run upon calling Slider()
     startSlider()
     applyClasses()
+    // move("back")
+    prevBtn.addEventListener('click', function () {
+        move('back')
+    })
+    nextBtn.addEventListener('click', function () {
+        move("forward")
+    })
 }
 // const test = 
 const mainSlider = Slider(document.querySelector('.main-slider'))
