2c8e2ca3be230dc41772c4bc6a9a3e236b75e304
Adam McWhorter <adamm2bits@hotmail.com>
2023-01-20 00:24:34 -0800

    daa jokes done

--- CORS/dadjoke/jokes.js
+++ CORS/dadjoke/jokes.js
@@ -2,42 +2,70 @@ const jokeBtn = document.querySelector('.get-joke')
 
 const jokeHolder = document.querySelector('.joke p ')
 
+const loader = document.querySelector('.lds-facebook')
+
 const buttonText = [
-    'Ugh.',
-    '🤦🏻‍♂️',
-    'omg dad.',
-    'you are the worst',
-    'seriously',
-    'stop it.',
-    'please stop',
-    'that was the worst one',
-  ];
-  
+	'Ugh.',
+	'🤦🏻‍♂️',
+	'omg dad.',
+	'you are the worst',
+	'seriously',
+	'stop it.',
+	'please stop',
+	'that was the worst one',
+]
+
+// const wait = (amount=0) => new Promise(resolve => setTimeout(resolve, amount))
+
+
 
 async function fetchJoke() {
-    jokeHolder.textContent = 'Loading...'
-    const response = await fetch('https://icanhazdadjoke.com', {
+    loader.classList.remove("hidden")
+    jokeBtn.classList.add('hidden')
+	const response = await fetch('https://icanhazdadjoke.com', {
         headers: {
             Accept: 'application/json',
-            
-        }
-    })
-    const data = await response.json()
-    console.log(data)
-    // return response.json()
-    return data
+		},
+	})
+	const data = await response.json()
+	console.log(data)
+	// return response.json()
+    loader.classList.add('hidden')
+    jokeBtn.classList.remove('hidden')
+    
+	return data
 }
 
 function handleError(err) {
-    console.log("we have an error!");
-    return new ErrorEvent(err)
+	console.log('we have an error!')
+	return new ErrorEvent(err)
 }
 
+function randomItemFromArray(arr, not) {
+	const item = arr[Math.floor(Math.random() * arr.length)]
+	// console.log(`not value:${not} \n item value: ${item}`);
+	if (not === item) {
+		console.log(`not value:${not} \n item value: ${item}`)
+		// item = arr[Math.floor(Math.random() * arr.length)]
+		return randomItemFromArray(arr, not)
+	}
+	return item
+}
+// randomItemFromArray(buttonText, jokeBtn.textContent)
 
+async function handleClick() {
+	const { joke } = await fetchJoke().catch(handleError)
+	// console.log(text);
+	jokeHolder.textContent = joke
+	console.log(joke)
+	jokeBtn.textContent = randomItemFromArray(buttonText, jokeBtn.textContent)
+}
+
+jokeBtn.addEventListener('click', handleClick)
 // console.log(fetchJoke().catch(handleError))
-jokeBtn.addEventListener('click', async function () {
+// jokeBtn.addEventListener('click', async function () {
 
-    const { joke } = await fetchJoke().catch(handleError) //NOTE:  { joke } to destructure data obj. and only access the joke property
-    console.log(joke);
-    jokeHolder.textContent = joke
-})
+//     const { joke } = await fetchJoke().catch(handleError) //NOTE:  { joke } to destructure data obj. and only access the joke property
+//     console.log(joke);
+//     jokeHolder.textContent = joke
+// })
