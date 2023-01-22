e4fa42612f17b0006c169c2b0525340bb4798dc9
Adam McWhorter <adamm2bits@hotmail.com>
2023-01-21 22:27:13 -0800

    need to figure out headers

--- currency-converter/converter.js
+++ currency-converter/converter.js
@@ -1,7 +1,14 @@
 // eslint-disable-next-line no-undef
-var myKey = config.API_KEY
+var access_key = config.access_key
 const myHeaders = new Headers()
-myHeaders.append("apikey", myKey)
+myHeaders.append("access_key", access_key)
+const requestOptions = {
+    method: "GET",
+    redirect: 'follow',
+    headers: myHeaders
+}
+
+const endpoint = 'https://api.exchangeratesapi.io/latest'
 
 const currencies = {
     USD: 'United States Dollar',
@@ -41,15 +48,30 @@ const currencies = {
 const fromSelect = document.querySelector('[name="from_currency"]')
 const toSelect = document.querySelector('[name="to_currency"]')
 
+// const wait = (amount=0) => Promise((resolve) => setTimeout(resolve, amount))
 function generateOptions(options) {
     return Object.entries(options).map(([currencyID, currencyName]) => {
         return `<option value="${currencyID}">${currencyID}-${currencyName}</option>`
     }).join('')
 }
 
+// function handleErrors(err) {
+//     console.log("uHH OH")
+//     console.log(err);
+// }
+async function fetchRates(base='USD') {
+    const res = await fetch(`${endpoint}?base=${base}`,requestOptions)
+    const data = await res.json()
+    console.log(data);
+
+    
+}
+
+
 const optionsHTML = generateOptions(currencies)
 
 fromSelect.innerHTML = optionsHTML
 toSelect.innerHTML = optionsHTML
 
+fetchRates('') //.catch(handleErrors)
 console.log(optionsHTML)
