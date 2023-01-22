4adef67bae2107ee2aa0ae2c949d54b2ac32efd5
Adam McWhorter <adamm2bits@hotmail.com>
2023-01-21 19:57:07 -0800

    testing

--- clickOutModal/rebuild/main.js
+++ clickOutModal/rebuild/main.js
@@ -1,17 +1,19 @@
 const cardBtns = document.querySelectorAll('.card button')
 
-function handleCardClick() {
-  // console.log(e.currentTarget)
-  console.log("clicked");
+const modalInner = document.querySelector('.modal-inner')
+const modalOuter = document.querySelector('.modal-outer')
+
+function handleCardClick(e) {
+	const button = e.currentTarget
+	const card = button.closest('.card')
+	console.log(card)
+	// const imgSrc = card.querySelector('img').src
+  const desc = card.dataset.description
+  console.log(desc)
+
+  
 }
 
 cardBtns.forEach((button) => {
 	button.addEventListener('click', handleCardClick)
-})
-
-const button = document.querySelector('.click')
-
-button.addEventListener('click', function () {
-  console.log("click")
-  
 })
---
+++ currency-converter/converter.js
@@ -0,0 +1,5 @@
+// eslint-disable-next-line no-undef
+var myKey = config.API_KEY
+const myHeaders = new Headers()
+myHeaders.append("apikey", myKey)
+console.log(myKey);
