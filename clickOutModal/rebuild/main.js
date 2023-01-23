953de6b810f6a0c616ac7f0fe20f0fdc175e6d72
Adam McWhorter <adamm2bits@hotmail.com>
2023-01-22 20:14:23 -0800

    had issues with clickoutmodal, must have been issues with livesever b/c its work correctly

--- clickOutModal/rebuild/main.js
+++ clickOutModal/rebuild/main.js
@@ -7,9 +7,13 @@ function handleCardClick(e) {
 	const button = e.currentTarget
 	const card = button.closest('.card')
 	console.log(card)
-	// const imgSrc = card.querySelector('img').src
-  const desc = card.dataset.description
-  console.log(desc)
+	const imgSrc = card.querySelector('img').src
+	const name = card.querySelector('h2').textContent
+	const desc = card.dataset.description
+	console.log(desc, name)
+	modalInner.innerHTML = `<img src="${imgSrc.replace('200', '600')}" alt="${name}">
+	<p>${desc}</p>`
+	modalOuter.classList.add('open')
 
   
 }
