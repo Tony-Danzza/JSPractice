1ea1cfb29b249b61976a81a69333e6aaa8e46133
Adam McWhorter <adamm2bits@hotmail.com>
2023-01-22 20:49:42 -0800

    clickOut rebuilt

--- clickOutModal/rebuild/main.js
+++ clickOutModal/rebuild/main.js
@@ -12,12 +12,33 @@ function handleCardClick(e) {
 	const desc = card.dataset.description
 	console.log(desc, name)
 	modalInner.innerHTML = `<img src="${imgSrc.replace('200', '600')}" alt="${name}">
-	<p>${desc}</p>`
+	<p>${desc ? desc : "No Description given"}</p>`
 	modalOuter.classList.add('open')
 
   
 }
 
+function closeModal() {
+	modalOuter.classList.remove('open')
+}
+
+modalOuter.addEventListener('click', function (e) {
+	// console.log(e.target)
+	const isOutside = !e.target.closest('.modal-inner')
+	// console.log(isOutside);
+	if (isOutside) {
+		closeModal()
+	}
+})
+
+window.addEventListener('keydown', (e) => {
+	// console.log(e.key)
+	if (e.key === "Escape") {
+		return closeModal()
+	}
+})
+
 cardBtns.forEach((button) => {
 	button.addEventListener('click', handleCardClick)
+
 })
