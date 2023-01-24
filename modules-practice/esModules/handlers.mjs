56f647fcf7f03da9c01656a88d692630ea30a865
Adam McWhorter <adamm2bits@hotmail.com>
2023-01-23 21:19:47 -0800

    modules update

---
+++ modules-practice/esModules/handlers.mjs
@@ -0,0 +1,5 @@
+export function sayHello(name) {
+    return `hello ${name}`
+}
+
+
---
+++ modules-practice/esModules/scripts.js
@@ -0,0 +1,9 @@
+import { sayHello } from "./handlers.mjs";
+const name = 'Adam'
+
+
+console.log(sayHello(name));
+// console.log(sayHi(name))
+
+console.log('its workings');
+// console.log(sayHi(name))
